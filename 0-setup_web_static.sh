e6485b33c4346aa9f85e0226bff37d979bc07c81
issahgordon <issahgordon3@gmail.com>
2023-04-06 04:22:03 -0700

    AirBnB_clone_v2

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,35 +1,37 @@
 #!/usr/bin/env bash
-# Sets up a web server for deployment of web_static.
+# Sets up your web servers for the deployment of web_static
+sudo apt-get -y update
 
-apt-get update
-apt-get install -y nginx
+# Install nginx
+sudo apt-get -y install nginx
+sudo ufw allow 'Nginx HTTP'
+# Create folders
+sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
 
-mkdir -p /data/web_static/releases/test/
-mkdir -p /data/web_static/shared/
-echo "Holberton School" > /data/web_static/releases/test/index.html
-ln -sf /data/web_static/releases/test/ /data/web_static/current
+# Create HMTL 
+sudo touch /data/web_static/releases/test/index.html
+echo "
+<html>
+ <head>
+   <title>
+   The title of your page
+   </title>
+ </head>
+ 
+ <body>
+   Your page content goes here.
+ </body>
+ </html>" > sudo /data/web_static/releases/test/index.html
 
-chown -R ubuntu /data/
-chgrp -R ubuntu /data/
+# Symbolic link
+sudo ln -sfn /data/web_static/releases/test/ /data/web_static/current
 
-printf %s "server {
-    listen 80 default_server;
-    listen [::]:80 default_server;
-    add_header X-Served-By $HOSTNAME;
-    root   /var/www/html;
-    index  index.html index.htm;
-    location /hbnb_static {
-        alias /data/web_static/current;
-        index index.html index.htm;
-    }
-    location /redirect_me {
-        return 301 http://cuberule.com/;
-    }
-    error_page 404 /404.html;
-    location /404 {
-      root /var/www/html;
-      internal;
-    }
-}" > /etc/nginx/sites-available/default
+# Create ownership
+sudo chown -R ubuntu:ubuntu /data/
 
-service nginx restart
+# Setup nginx
+sudo sed -i '/listen 80 default_server/a location /hbnb_static/ {alias /data/web_static/current/;}' /etc/nginx/sites-available/default
+
+#Restart nginx
+sudo service nginx restart
