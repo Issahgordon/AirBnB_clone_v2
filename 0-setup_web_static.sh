6523b73ab6a5d673314dd88e1f85bb15db620c6f
issahgordon <issahgordon3@gmail.com>
2023-04-07 01:46:03 -0700

    AirBnB_clone_v2

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,57 +1,27 @@
 #!/usr/bin/env bash
-# script to install and setup nginx
-CONFIG_FILE="/etc/nginx/sites-available/default"
-HOST_NAME=$(hostname)
-MY_ID=496
-STATIC=/data/web_static
 
-# check if hostname is correct
-if [[ $(hostname) =~ ^$MY_ID-web-[0-9]+ ]]; then
-    echo 'hostname properly configured'
-else
-    (>&2 echo 'hostname not configured properly...')
-    (>&2 echo 'please set hostname to pattern: 496-web-<server_id>...')
-    (>&2 echo 'Example: sudo hostnamectl set-hostname 496-web-<insert_server_id_here>')
+# Install Nginx if it's not already installed
+if ! [ -x "$(command -v nginx)" ]; then
+    sudo apt-get update
+    sudo apt-get -y install nginx
 fi
 
-# install nginx
-apt-get -y update
-apt-get -y install nginx
+# Create necessary directories
+sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
 
-# update 404 error page
-echo "Ceci n'est pas une page" > /usr/share/nginx/html/404.html
+# Create fake HTML file for testing
+echo "<html><head></head><body>Holberton School is awesome!</body></html>" | sudo tee /data/web_static/releases/test/index.html
 
-# create static directories and links
-mkdir -p $STATIC/releases/test
-mkdir -p $STATIC/shared
-echo 'Holberton School Is Running!' > $STATIC/releases/test/index.html
-ln -sfn $STATIC/releases/test $STATIC/current
-sudo chown -f -R ubuntu:ubuntu /data/
+# Create symbolic link and give ownership to ubuntu user and group
+sudo rm -f /data/web_static/current
+sudo ln -s /data/web_static/releases/test/ /data/web_static/current
+sudo chown -R ubuntu:ubuntu /data/
 
-# update config file to redirect
-printf %s "server {
-    listen 80;
-    listen [::]:80 default_server;
-    root   $STATIC/current;
-    index  index.html index.htm 8-index.html;
-    add_header X-Served-By $HOST_NAME;
-    location / {
-        alias $STATIC/current/;
-    }
-    location /redirect_me {
-        return 301 http://google.com/;
-    }
-    location /hbnb_static {
-        alias $STATIC/current/;
-    }
-    error_page 404 /404.html;
-    location /404 {
-      root /usr/share/nginx/html;
-      internal;
-    }
-}" > $CONFIG_FILE
+# Update Nginx configuration
+location_alias="location /hbnb_static {\n\talias /data/web_static/current/;\n}\n"
+sudo sed -i "/server_name _;/a $location_alias" /etc/nginx/sites-available/default
 
-# start nginx after reloading config
-service nginx start
-# if nginx was already running restart it
-service nginx restart
+# Restart Nginx
+sudo service nginx restart
+
