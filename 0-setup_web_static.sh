aefed4f3e6e501e863025a4c860f428af6f76f49
issahgordon <issahgordon3@gmail.com>
2023-04-07 01:18:58 -0700

    AirBnB_clone_v2

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,37 +1,57 @@
 #!/usr/bin/env bash
-# Sets up your web servers for the deployment of web_static
-sudo apt-get -y update
+# script to install and setup nginx
+CONFIG_FILE="/etc/nginx/sites-available/default"
+HOST_NAME=$(hostname)
+MY_ID=496
+STATIC=/data/web_static
 
-# Install nginx
-sudo apt-get -y install nginx
-sudo ufw allow 'Nginx HTTP'
-# Create folders
-sudo mkdir -p /data/web_static/releases/test/
-sudo mkdir -p /data/web_static/shared/
+# check if hostname is correct
+if [[ $(hostname) =~ ^$MY_ID-web-[0-9]+ ]]; then
+    echo 'hostname properly configured'
+else
+    (>&2 echo 'hostname not configured properly...')
+    (>&2 echo 'please set hostname to pattern: 496-web-<server_id>...')
+    (>&2 echo 'Example: sudo hostnamectl set-hostname 496-web-<insert_server_id_here>')
+fi
 
-# Create HMTL 
-sudo touch /data/web_static/releases/test/index.html
-echo "
-<html>
- <head>
-   <title>
-   The title of your page
-   </title>
- </head>
- 
- <body>
-   Your page content goes here.
- </body>
- </html>" > sudo /data/web_static/releases/test/index.html
+# install nginx
+apt-get -y update
+apt-get -y install nginx
 
-# Symbolic link
-sudo ln -sfn /data/web_static/releases/test/ /data/web_static/current
+# update 404 error page
+echo "Ceci n'est pas une page" > /usr/share/nginx/html/404.html
 
-# Create ownership
-sudo chown -R ubuntu:ubuntu /data/
+# create static directories and links
+mkdir -p $STATIC/releases/test
+mkdir -p $STATIC/shared
+echo 'Holberton School Is Running!' > $STATIC/releases/test/index.html
+ln -sfn $STATIC/releases/test $STATIC/current
+sudo chown -f -R ubuntu:ubuntu /data/
 
-# Setup nginx
-sudo sed -i '/listen 80 default_server/a location /hbnb_static/ {alias /data/web_static/current/;}' /etc/nginx/sites-available/default
+# update config file to redirect
+printf %s "server {
+    listen 80;
+    listen [::]:80 default_server;
+    root   $STATIC/current;
+    index  index.html index.htm 8-index.html;
+    add_header X-Served-By $HOST_NAME;
+    location / {
+        alias $STATIC/current/;
+    }
+    location /redirect_me {
+        return 301 http://google.com/;
+    }
+    location /hbnb_static {
+        alias $STATIC/current/;
+    }
+    error_page 404 /404.html;
+    location /404 {
+      root /usr/share/nginx/html;
+      internal;
+    }
+}" > $CONFIG_FILE
 
-#Restart nginx
-sudo service nginx restart
+# start nginx after reloading config
+service nginx start
+# if nginx was already running restart it
+service nginx restart
